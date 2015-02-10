291dc9adfc173723d400af4e905c4c2ec5060deb
角田 均 <tsunoda@aomori-u.ac.jp>
2015-02-10 17:01:43 +0900

    p125

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -4,7 +4,25 @@ class LessonController < ApplicationController
   def step1
     render text: "こんにちは、#{params[:name]}さん"
   end
+
   def step2
     render text: params[:controller] + "#" + params[:action]
+  end
+
+  def step3
+    redirect_to action: "step4"
+  end
+
+  def step4
+    render text: "step4に移動しました。"
+  end
+
+  def step5
+    flash[:notice] = "step6に移動します。"
+    redirect_to action: "step6"
+  end
+
+  def step6
+    render text: flash[:notice]
   end
 end
